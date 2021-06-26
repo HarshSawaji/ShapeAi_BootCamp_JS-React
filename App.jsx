bec6675bd3cdfd689d053f4f30a69c96ee14a752
HarshSawaji <86519686+HarshSawaji@users.noreply.github.com>
2021-06-26 17:22:50 +0530

    First commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Info from './Info';
+import Footer from './Footer';
+import Header from './Header';
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Info />
+      <Info />
+      <Footer/>
+    </div>
+  );
+}
---
+++ Info.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Info(props) {
+  return (
+    <div className="note">
+      <h1>React and JavaScript</h1>
+      <p>7 Days Free Bootcamp on Basic Web Dev  in collaboration withGoogle Developer Groups,  Microsoft Learn Student Ambassador Program, and AWS Students Club</p>
+    </div>
+  );
+}
